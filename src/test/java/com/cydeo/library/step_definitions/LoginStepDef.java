17b0ac86f6579852cfa6d2c52ccb54f1b029fd1e
Ali Kodirov <malikodirov@gmail.com>
2022-11-05 05:29:56 -0500

    Step def classes created

---
+++ src/test/java/com/cydeo/library/step_definitions/LoginStepDef.java
@@ -0,0 +1,19 @@
+package com.cydeo.library.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+    }
+
+    @When("I login as a librarian")
+    public void iLoginAsALibrarian() {
+    }
+
+    @Then("dashboard should be displayed")
+    public void dashboardShouldBeDisplayed() {
+    }
+}
